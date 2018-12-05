59990ef61d544edf54d13aa4ad0d58f5d23b9a1e
Doychin Atanasov <ironsmile@gmail.com>
2018-12-05 16:53:00 +0200

    Initial commit

---
+++ docs.go
@@ -0,0 +1,22 @@
+/*
+Package moons provides information about the moons of the planets in the solar system.
+
+University Stuff
+
+This is an example package used during the Go Programming course at the Faculty of
+Mathematics and Informatics at Sofia University "Kliment Ohridski". It is used for
+examples during the course.
+
+Usage
+
+The killer feature of this package is that it will allow you to show how many moons
+a planet in the solar system has:
+
+    jupiterMoons, err := moons.Count(moons.BodyFromString("jupiter"))
+
+Disclaimers
+
+Information provided in this package may not be accurate. We're just a bunch of
+lecturers and have little to do with astronomy.
+*/
+package moons
---
+++ moons.go
@@ -0,0 +1,72 @@
+package moons
+
+import (
+	"strings"
+
+	"fmt"
+)
+
+// CelestialBody represents a natural body in our solar system.
+//go:generate stringer -type=CelestialBody
+type CelestialBody int
+
+// Constants with the most prominent celestial bodies which this library understands.
+const (
+	Sun CelestialBody = iota
+	Mercury
+	Venus
+	Earth
+	Mars
+	Jupiter
+	Saturn
+	Uranus
+	Neptune
+
+	// Unknown is used when converting from string to CelestialBody and shows that
+	// the returned body is not recognized.
+	Unknown
+)
+
+// Count returns the number of moons a CelestialBody planet has.
+func Count(planet CelestialBody) (int, error) {
+	if planet == Unknown {
+		return 0, fmt.Errorf("unknown CelestialBody: %s", planet)
+	}
+
+	count, ok := moonsCount[planet]
+	if !ok {
+		return 0, fmt.Errorf("%s is not a planet", planet)
+	}
+	return count, nil
+}
+
+// BodyFromString returns a CelestialBody which corresponds to the string `name`.
+func BodyFromString(name string) CelestialBody {
+	body, ok := nameToBody[strings.ToLower(name)]
+	if !ok {
+		return Unknown
+	}
+
+	return body
+}
+
+var moonsCount = map[CelestialBody]int{
+	Mercury: 0,
+	Venus:   0,
+	Earth:   1,
+	Jupiter: 67,
+	Saturn:  62,
+	Uranus:  27,
+	Neptune: 14,
+}
+
+var nameToBody = map[string]CelestialBody{
+	"sun":     Sun,
+	"mercury": Mercury,
+	"venus":   Venus,
+	"earth":   Earth,
+	"jupiter": Jupiter,
+	"saturn":  Saturn,
+	"uranus":  Uranus,
+	"neptune": Neptune,
+}
